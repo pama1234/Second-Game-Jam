c0b0674d963dcccbb48fb9874e6cf0dfef56d79a
OFFMIND <1127198066@qq.com>
2023-07-04 03:17:13 +0800

    2023.7.4

---
+++ Assets/Manager/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : Singleton<GameManager>
+{
+    //Fader还没开始用，直接检测罢（。。）
+    string originScene;
+    string nowScene;
+    private void Start()
+    {
+        originScene = SceneManager.GetActiveScene().name;
+        nowScene = SceneManager.GetActiveScene().name;
+    }
+    private void Update()
+    {
+        nowScene = SceneManager.GetActiveScene().name;
+        if(nowScene != originScene)
+        {
+            originScene = nowScene;
+            DialogManager.Instance.Init();
+        }
+    }
+}
---
+++ Assets/Manager/SaveSystem/FileAccess.cs
@@ -0,0 +1,8 @@
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+public static class FileAccess 
+{
+
+}
