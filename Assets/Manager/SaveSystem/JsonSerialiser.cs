b45d28653aa583ee20ad0b5e7950482771d970ad
OFFMIND <1127198066@qq.com>
2023-07-04 17:26:22 +0800

    没试过

---
+++ Assets/Manager/SaveSystem/JsonSerialiser.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public static class JsonSerialiser
+{
+    private static readonly JsonSerializerSettings _settings = new JsonSerializerSettings();
+    private static JsonSerializer _serializer = JsonSerializer.Create(_settings);
+    public static string Serialize<T>(T value)
+    {
+        return JsonConvert.SerializeObject(value, _settings);
+    }
+    public static JToken SerializeKey<T>(T key)
+    {
+        return JToken.FromObject(key, _serializer);    }
+
+}
